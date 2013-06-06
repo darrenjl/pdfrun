e9cba5edb5ad22f72e772931e4de473743b727a8
darrenjl <d.lyons88@gmail.com>
2013-06-06 14:27:48 +0100

    displaying best times for significant distances in profile activity

--- OpenGPSTracker/application/src/com/pdfrun/activity/ProfileActivity.java
+++ OpenGPSTracker/application/src/com/pdfrun/activity/ProfileActivity.java
@@ -16,6 +16,8 @@ import android.content.ContentUris;
 import android.database.Cursor;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class ProfileActivity extends Activity implements StatisticsDelegate
@@ -27,6 +29,21 @@ public class ProfileActivity extends Activity implements StatisticsDelegate
    private UnitsI18n mUnits;
    private float totalDistance=0;
    private double maxSpeed=0;
+   private long best1kmTime=0;
+   private long best3kmTime=0;
+   private long best5kmTime=0;
+   private long best8kmTime=0;
+   private long best10kmTime=0;
+   private TextView km1TimeView;
+   private TextView km3TimeView;
+   private TextView km5TimeView;
+   private TextView km8TimeView;
+   private TextView km10TimeView;
+   private TableRow km1TimeTableRow;
+   private TableRow km3TimeTableRow;
+   private TableRow km5TimeTableRow;
+   private TableRow km8TimeTableRow;
+   private TableRow km10TimeTableRow;
 
    @Override
    protected void onCreate(Bundle savedInstanceState)
@@ -36,6 +53,16 @@ public class ProfileActivity extends Activity implements StatisticsDelegate
       numRunsView = (TextView)findViewById(R.id.numRuns);
       totalDistanceView = (TextView)findViewById(R.id.totalDistance);
       maxSpeedView = (TextView)findViewById(R.id.maxSpeed);
+      km1TimeView = (TextView) findViewById(R.id.km_1_time);
+      km3TimeView = (TextView) findViewById(R.id.km_3_time);
+      km5TimeView = (TextView) findViewById(R.id.km_5_time);
+      km8TimeView = (TextView) findViewById(R.id.km_8_time);
+      km10TimeView = (TextView) findViewById(R.id.km_10_time);
+      km1TimeTableRow   = (TableRow) findViewById(R.id.km_1_table_row);
+      km3TimeTableRow   = (TableRow) findViewById(R.id.km_3_table_row);
+      km5TimeTableRow   = (TableRow) findViewById(R.id.km_5_table_row);
+      km8TimeTableRow   = (TableRow) findViewById(R.id.km_8_table_row);
+      km10TimeTableRow   = (TableRow) findViewById(R.id.km_10_table_row);
       mUnits = new UnitsI18n( this, new UnitsI18n.UnitsChangeListener()
       {
          @Override
@@ -54,26 +81,18 @@ public class ProfileActivity extends Activity implements StatisticsDelegate
    }
    
    private void updateUI()
-   {
-      
-      numRunsView.setText(Integer.toString(getNumRuns()));
-      getTotalDistance();
+   {      
+      getRunStatistics();
    }
    
-   private int getNumRuns(){
+   private void getRunStatistics(){
       tracksCursor = managedQuery(Tracks.CONTENT_URI, new String[] { Tracks._ID, Tracks.NAME, Tracks.CREATION_TIME }, null, null, Tracks.CREATION_TIME + " DESC");
-      int numRows = tracksCursor.getCount();
-      tracksCursor.close();
-      return numRows;
-   }
-   
-   private void getTotalDistance(){
-      tracksCursor = managedQuery(Tracks.CONTENT_URI, new String[] { Tracks._ID, Tracks.NAME, Tracks.CREATION_TIME }, null, null, Tracks.CREATION_TIME + " DESC");
+      numRunsView.setText(Integer.toString(tracksCursor.getCount()));
       tracksCursor.moveToFirst();
       while (tracksCursor.isAfterLast() == false) 
       {
          int id = tracksCursor.getInt(0);
-          Log.d("pdfrun", "track id: "+ id);
+          Log.d("PDFRun", "track id: "+ id);
           tracksCursor.moveToNext();
           StatisticsCalulator calculator = new StatisticsCalulator( this, mUnits, this );
           Uri trackUri = ContentUris.withAppendedId(Tracks.CONTENT_URI, id);
@@ -91,6 +110,41 @@ public class ProfileActivity extends Activity implements StatisticsDelegate
       if(calculated.getMaxSpeed()>maxSpeed){
          maxSpeed=calculated.getMaxSpeed();
          maxSpeedView.setText(calculated.getMaxSpeedText());
+      }
+      if(calculated.getKm1Time()>0){
+         if(calculated.getKm1Time()<best1kmTime||best1kmTime==0){
+            best1kmTime=calculated.getKm1Time();
+            km1TimeView.setText(calculated.getKm1TimeText());
+         }
+         km1TimeTableRow.setVisibility(View.VISIBLE);
+      }
+      if(calculated.getKm3Time()>0){
+         if(calculated.getKm3Time()<best3kmTime||best3kmTime==0){
+            best3kmTime=calculated.getKm3Time();
+            km3TimeView.setText(calculated.getKm3TimeText());
+         }
+         km3TimeTableRow.setVisibility(View.VISIBLE);
+      }
+      if(calculated.getKm5Time()>0){
+         if(calculated.getKm5Time()<best5kmTime||best5kmTime==0){
+            best5kmTime=calculated.getKm5Time();
+            km5TimeView.setText(calculated.getKm5TimeText());
+         }
+         km5TimeTableRow.setVisibility(View.VISIBLE);
+      }
+      if(calculated.getKm8Time()>0){
+         if(calculated.getKm8Time()<best8kmTime||best8kmTime==0){
+            best8kmTime=calculated.getKm8Time();
+            km8TimeView.setText(calculated.getKm8TimeText());
+         }
+         km8TimeTableRow.setVisibility(View.VISIBLE);
+      }
+      if(calculated.getKm10Time()>0){
+         if(calculated.getKm10Time()<best10kmTime||best10kmTime==0){
+            best10kmTime=calculated.getKm10Time();
+            km10TimeView.setText(calculated.getKm10TimeText());
+         }
+         km10TimeTableRow.setVisibility(View.VISIBLE);
       }
    }
 }
