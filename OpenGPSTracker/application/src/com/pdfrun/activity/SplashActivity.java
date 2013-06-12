4e5181f1887d4f03fff08992e7884898567f0cdf
darrenjl <d.lyons88@gmail.com>
2013-06-12 01:33:05 +0100

    Adding splash screen functionality. Just displays blank screen for now, need to create image

---
+++ OpenGPSTracker/application/src/com/pdfrun/activity/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.pdfrun.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.pdfrun.R;
+
+public class SplashActivity extends Activity {
+
+   private static String TAG = SplashActivity.class.getName();
+   private static long SLEEP_TIME = 3;    
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    this.requestWindowFeature(Window.FEATURE_NO_TITLE);   
+    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,     WindowManager.LayoutParams.FLAG_FULLSCREEN);   
+
+    setContentView(R.layout.activity_splash);
+
+    IntentLauncher launcher = new IntentLauncher();
+    launcher.start();
+  }
+   
+   private class IntentLauncher extends Thread {
+    @Override
+    public void run() {
+       try {
+          Thread.sleep(SLEEP_TIME*1000);
+       } catch (Exception e) {
+          Log.e(TAG, e.getMessage());
+       }
+       Intent intent = new Intent(SplashActivity.this, DashboardActivity.class);
+       SplashActivity.this.startActivity(intent);
+       SplashActivity.this.finish();
+    }
+  }
+}
