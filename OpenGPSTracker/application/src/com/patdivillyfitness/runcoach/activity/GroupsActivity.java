3c41e087ac4fd45d2f96513cd858ff97fc176663
darrenjl <d.lyons88@gmail.com>
2013-06-21 14:30:49 +0100

    Changing the display of goals to a single page table format

--- OpenGPSTracker/application/src/com/patdivillyfitness/runcoach/activity/GroupsActivity.java
+++ OpenGPSTracker/application/src/com/patdivillyfitness/runcoach/activity/GroupsActivity.java
@@ -2,58 +2,90 @@ package com.patdivillyfitness.runcoach.activity;
 
 import java.util.ArrayList;
 
-import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ListView;
+import android.widget.TextView;
 
-import com.actionbarsherlock.app.SherlockListActivity;
-import com.patdivillyfitness.runcoach.Constants;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.patdivillyfitness.runcoach.PDFUtil;
 import com.patdivillyfitness.runcoach.R;
-import com.patdivillyfitness.runcoach.adapter.MySimpleArrayAdapter;
 
-public class GroupsActivity extends SherlockListActivity
+public class GroupsActivity extends SherlockActivity
 {
    private ArrayList<TypedArray> values;
+   private int[] goals;
+   private TextView goal1KmBronze;
+   private TextView goal1KmSilver;
+   private TextView goal1KmGold;
+   
+   private TextView goal3KmBronze;
+   private TextView goal3KmSilver;
+   private TextView goal3KmGold;
+   
+   private TextView goal5KmBronze;
+   private TextView goal5KmSilver;
+   private TextView goal5KmGold;
+   
+   private TextView goal8KmBronze;
+   private TextView goal8KmSilver;
+   private TextView goal8KmGold;
+   
+   private TextView goal10KmBronze;
+   private TextView goal10KmSilver;
+   private TextView goal10KmGold;
 
    @Override
    protected void onCreate(Bundle savedInstanceState)
    {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_groups);
-      initialiseData();
-      MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
-      setListAdapter(adapter);
-   }
-
-   @Override
-   protected void onListItemClick(ListView l, View v, int position, long id)
-   {
-      Intent intent = new Intent(this, GroupDetailsActivity.class);
-      intent.putExtra(Constants.BRONZE_GOAL, values.get(position).getInt(1,0));
-      intent.putExtra(Constants.SILVER_GOAL, values.get(position).getInt(2,0));
-      intent.putExtra(Constants.GOLD_GOAL, values.get(position).getInt(3,0));
-      startActivity(intent);
-   }
-
-   private void initialiseData()
-   {
-      values = new ArrayList<TypedArray>();
-      Resources res = getResources();
-      values.add(res.obtainTypedArray(R.array.goals_1km));
-      values.add(res.obtainTypedArray(R.array.goals_3km));
-      values.add(res.obtainTypedArray(R.array.goals_5km));
-      values.add(res.obtainTypedArray(R.array.goals_8km));
-      values.add(res.obtainTypedArray(R.array.goals_10km));
+      
+      goal1KmBronze = (TextView) findViewById(R.id.bronze_km_1_time);
+      goal1KmSilver = (TextView) findViewById(R.id.silver_km_1_time);
+      goal1KmGold = (TextView) findViewById(R.id.gold_km_1_time);
+      
+      goal3KmBronze = (TextView) findViewById(R.id.bronze_km_3_time);
+      goal3KmSilver = (TextView) findViewById(R.id.silver_km_3_time);
+      goal3KmGold = (TextView) findViewById(R.id.gold_km_3_time);
+      
+      goal5KmBronze = (TextView) findViewById(R.id.bronze_km_5_time);
+      goal5KmSilver = (TextView) findViewById(R.id.silver_km_5_time);
+      goal5KmGold = (TextView) findViewById(R.id.gold_km_5_time);
+      
+      goal8KmBronze = (TextView) findViewById(R.id.bronze_km_8_time);
+      goal8KmSilver = (TextView) findViewById(R.id.silver_km_8_time);
+      goal8KmGold = (TextView) findViewById(R.id.gold_km_8_time);
+      
+      goal10KmBronze = (TextView) findViewById(R.id.bronze_km_10_time);
+      goal10KmSilver = (TextView) findViewById(R.id.silver_km_10_time);
+      goal10KmGold = (TextView) findViewById(R.id.gold_km_10_time);
+      Resources res = getResources();      
+      goals=res.getIntArray(R.array.goals_1km);
+      goal1KmBronze.setText(PDFUtil.getTimeText(goals[0]));
+      goal1KmSilver.setText(PDFUtil.getTimeText(goals[1]));
+      goal1KmGold.setText(PDFUtil.getTimeText(goals[2]));
+      
+      goals=res.getIntArray(R.array.goals_3km);
+      goal3KmBronze.setText(PDFUtil.getTimeText(goals[0]));
+      goal3KmSilver.setText(PDFUtil.getTimeText(goals[1]));
+      goal3KmGold.setText(PDFUtil.getTimeText(goals[2]));
+      
+      goals=res.getIntArray(R.array.goals_5km);
+      goal5KmBronze.setText(PDFUtil.getTimeText(goals[0]));
+      goal5KmSilver.setText(PDFUtil.getTimeText(goals[1]));
+      goal5KmGold.setText(PDFUtil.getTimeText(goals[2]));
+      
+      goals=res.getIntArray(R.array.goals_8km);
+      goal8KmBronze.setText(PDFUtil.getTimeText(goals[0]));
+      goal8KmSilver.setText(PDFUtil.getTimeText(goals[1]));
+      goal8KmGold.setText(PDFUtil.getTimeText(goals[2]));
+      
+      goals=res.getIntArray(R.array.goals_10km);
+      goal10KmBronze.setText(PDFUtil.getTimeText(goals[0]));
+      goal10KmSilver.setText(PDFUtil.getTimeText(goals[1]));
+      goal10KmGold.setText(PDFUtil.getTimeText(goals[2]));
    }
    
-   @Override
-   protected void onDestroy()
-   {
-      for (TypedArray arr:values)
-         arr.recycle();
-      super.onDestroy();
+
    }
-}
