e753e735a7a8199dd389134e31bb85d3902e70a2
darrenjl <d.lyons88@gmail.com>
2013-06-22 22:03:35 +0100

    Moving strings from AppLaunchChecker to resources

--- OpenGPSTracker/application/src/com/patdivillyfitness/runcoach/AppLaunchChecker.java
+++ OpenGPSTracker/application/src/com/patdivillyfitness/runcoach/AppLaunchChecker.java
@@ -21,20 +21,16 @@ public class AppLaunchChecker {
        SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
        if (prefs.getBoolean("dontshowagain", false)) { return true; }
        
-       SharedPreferences.Editor editor = prefs.edit();
-       
-       // Increment launch counter
+       SharedPreferences.Editor editor = prefs.edit();       
        long launch_count = prefs.getLong("launch_count", 0) + 1;
        editor.putLong("launch_count", launch_count);
 
-       // Get date of first launch
        Long date_firstLaunch = prefs.getLong("date_firstlaunch", 0);
        if (date_firstLaunch == 0) {
            date_firstLaunch = System.currentTimeMillis();
            editor.putLong("date_firstlaunch", date_firstLaunch);
        }
        
-       // Wait at least n days before opening
        if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
            if (System.currentTimeMillis() >= date_firstLaunch + 
                    (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
@@ -54,22 +50,21 @@ public class AppLaunchChecker {
 
       AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
 
-      alertDialog.setTitle("Rate " + APP_TITLE);
+      alertDialog.setTitle(String.format(mContext.getString(R.string.rate_app), APP_TITLE));
+      alertDialog.setMessage(String.format(mContext.getString(R.string.rate_app_text), APP_TITLE));
 
-      alertDialog.setMessage("If you enjoy using " + APP_TITLE + ", please take a moment to rate it. Thanks for your support!");
-
-      alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Rate", new DialogInterface.OnClickListener()
+      alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, mContext.getString(R.string.rate_app), new DialogInterface.OnClickListener()
          {
 
             public void onClick(DialogInterface dialog, int id)
             {
                //              mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + APP_PNAME)));
-               Toast.makeText(mContext, "Google Play will be launched, once the app is on the market", Toast.LENGTH_LONG).show();
+               Toast.makeText(mContext, mContext.getString(R.string.google_play_launched), Toast.LENGTH_LONG).show();
                dialog.dismiss();
             }
          });
 
-      alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Remind", new DialogInterface.OnClickListener()
+      alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, mContext.getString(R.string.remind), new DialogInterface.OnClickListener()
          {
 
             public void onClick(DialogInterface dialog, int id)
@@ -80,7 +75,7 @@ public class AppLaunchChecker {
             }
          });
 
-      alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "No", new DialogInterface.OnClickListener()
+      alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, mContext.getString(R.string.no), new DialogInterface.OnClickListener()
          {
 
             public void onClick(DialogInterface dialog, int id)
