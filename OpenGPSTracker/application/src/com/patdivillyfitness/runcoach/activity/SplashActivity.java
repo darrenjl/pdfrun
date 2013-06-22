8c159d932044833f536bb534120a4ce663a139d8
darrenjl <d.lyons88@gmail.com>
2013-06-22 11:33:09 +0100

    Adding db dump function to splash activity, commented out call. Will only be called in development mode when required

--- OpenGPSTracker/application/src/com/patdivillyfitness/runcoach/activity/SplashActivity.java
+++ OpenGPSTracker/application/src/com/patdivillyfitness/runcoach/activity/SplashActivity.java
@@ -1,11 +1,17 @@
 package com.patdivillyfitness.runcoach.activity;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.patdivillyfitness.runcoach.R;
 
@@ -22,7 +28,7 @@ public class SplashActivity extends Activity {
     this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,     WindowManager.LayoutParams.FLAG_FULLSCREEN);   
 
     setContentView(R.layout.activity_splash);
-
+//    copyDBToSD();
     IntentLauncher launcher = new IntentLauncher();
     launcher.start();
   }
@@ -40,4 +46,41 @@ public class SplashActivity extends Activity {
        SplashActivity.this.finish();
     }
   }
+   
+   public void copyDBToSD(){
+      File f=new File("/data/data/com.patdivillyfitness.runcoach/databases/GPSLOG.db");
+      FileInputStream fis=null;
+      FileOutputStream fos=null;
+
+      try
+      {
+        fis=new FileInputStream(f);
+        fos=new FileOutputStream("/mnt/sdcard/db_dump.dbs");
+        while(true)
+        {
+          int i=fis.read();
+          if(i!=-1)
+          {fos.write(i);}
+          else
+          {break;}
+        }
+        fos.flush();
+        Toast.makeText(this, "DB dump OK", Toast.LENGTH_LONG).show();
+      }
+      catch(Exception e)
+      {
+        e.printStackTrace();
+        Toast.makeText(this, "DB dump ERROR", Toast.LENGTH_LONG).show();
+      }
+      finally
+      {
+        try
+        {
+          fos.close();
+          fis.close();
+        }
+        catch(IOException ioe)
+        {}
+      }
+   }
 }
