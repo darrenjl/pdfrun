8218ce17b231f38ccf7b013b954b7b9b0f116eea
darrenjl <d.lyons88@gmail.com>
2013-07-25 13:44:25 +0100

    Adding facebook login feature

---
+++ OpenGPSTracker/application/src/com/patdivillyfitness/runcoach/fragment/LoginFragment.java
@@ -0,0 +1,17 @@
+package com.patdivillyfitness.runcoach.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.patdivillyfitness.runcoach.R;
+
+public class LoginFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        return view;
+    }
+}
